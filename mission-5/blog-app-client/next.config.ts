febcd611661c55df3ed126bc7af0580644dce4ab
dev-mhratul <dev.mhratul@gmail.com>
2026-04-29 18:07:18 +0600

    learn proxy.ts

--- mission-5/blog-app-client/next.config.ts
+++ mission-5/blog-app-client/next.config.ts
@@ -1,3 +1,5 @@
+
+// import './src'
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
--- mission-5/blog-app-client/src/services/user.service.ts
+++ mission-5/blog-app-client/src/services/user.service.ts
@@ -1,11 +1,15 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
 
+const AUTH_URL= env.AUTH_URL
+
 export const userService = {
+
     getSelection: async function () {
         try {
             const cookieStore = await cookies()
 
-            const res = await fetch("http://localhost:5000/api/auth/get-session", {
+            const res = await fetch(`${AUTH_URL}/get-session`, {
                 headers: {
                     Cookie: cookieStore.toString(),
                 },
